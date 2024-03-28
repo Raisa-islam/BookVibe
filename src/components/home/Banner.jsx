ba51afcedde59d8603dbe86da907d734dd85ae0d
Raisa-islam <raisa.isl683@gmail.com>
2024-03-28 22:25:05 +0600

    navigate to view the list done

--- src/components/home/Banner.jsx
+++ src/components/home/Banner.jsx
@@ -1,10 +1,16 @@
+import { useNavigate } from 'react-router-dom';
 import banImg from '../../assets/pngwing 1.png'
 const Banner = () => {
+    const navigate = useNavigate();
+
+    const handleViewList = () => {
+        navigate('/listed-books');
+    };
     return (
         <div className='rounded-3xl bg-[#13131305] flex flex-row justify-between items-center p-4 md:p-24 mt-12 text-black'>
             <div>
                 <p className='text-2xl md:text-4xl lg:text-6xl font-bold'>Books to freshen up your bookshelf</p>
-                <button className='btn btn-primary bg-[#23BE0A] rounded-lg border-0 text-white mt-7 hover:bg-green-600'>View The List</button>
+                <button className='btn btn-primary bg-[#23BE0A] rounded-lg border-0 text-white mt-7 hover:bg-green-600' onClick={handleViewList}>View The List</button>
             </div>
             <div>
                 <img src={banImg} alt="" className='w-64 h-64' />
